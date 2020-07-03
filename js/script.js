9393a9529951b636e0ea5572572958745cde085a
Dmitry <yanyushin.dim@gmail.com>
2020-07-03 17:56:09 +0300

    циклы

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 "use strict";
 
+let num = 50;
+
+//while (num <= 55) {
+   // console.log(num);
+    //num++;
+//}
+
+//do {
+    //console.log(num);
+    //num++; 
+//}
+//while (num <= 55);
+
+for (let i = 1; i < 0; i++) {
+    console.log(num);
+    num++;
+}
