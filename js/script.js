b3a30d2d2850d39e7c348c8b9be337f0cc531c18
Dmitry <yanyushin.dim@gmail.com>
2020-07-08 19:00:08 +0300

    Object

--- js/script.js
+++ js/script.js
@@ -1,37 +1,41 @@
 "use strict";
 
-function first() {
-    // Do something
-    setTimeout(function(){
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    collors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
+options.makeTest();
 
-first();
-second();
+const {border, bg} = options.collors;
+console.log(border);
 
-function learnJS (lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+// console.log(Object.keys(options).length);
 
-learnJS(`JavaScript`, function() {
-    console.log('Я прошёл этот урок');
-});
+// console.log(options.name);
 
+// delete options.name;
 
+// console.log(options);
 
-
-function learnJS (lang, callback) {
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
-function done () {
-    console.log('Я прошёл этот урок!');
-}
-
-learnJS(`JavaScript`, done);
+// let counter = 0;
+// for (let key in options) {
+//     if(typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойства ${i} имеет значение ${options[key][i]}`);
+//             counter++;
+//         }
+//     }else {
+//         console.log(`Свойства ${key} имеет значение ${options[key]}`);
+//         counter++;
+//     }
+// }
+// console.log(counter);
